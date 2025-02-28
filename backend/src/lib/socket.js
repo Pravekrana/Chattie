a4d67fa3cdc14146f83e9c5b48523456db01eb6f
Pravekrana <89938957+Pravekrana@users.noreply.github.com>
2025-02-28 15:52:59 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,11 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173", // Local development
-      "https://your-netlify-site.netlify.app", // Frontend after deployment
-    ],
-    methods: ["GET", "POST"],
+    origin: ["http://localhost:5173"],
   },
 });
 
